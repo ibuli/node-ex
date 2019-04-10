e5e3ec7dccbde1f00d241e01ea88341cea3bfc5b
Burhan <burhan@200oksolutions.com>
2019-04-10 12:35:30 +0530

    - add mongoose and bodyParser libraries - add Schema for the Contact in crmModel

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
 import routes from './src/routes/crmRoutes'
 
 const app = express()
 const PORT = 3000
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+  /* DeprecationWarning: current URL string parser is deprecated, 
+     and will be removed in a future version. To use the new parser, 
+     pass option { useNewUrlParser: true } to MongoClient.connect.
+  */
+  useNewUrlParser: true
+})
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 routes(app)
 
